75e869bd7fcc845d33feeec2d8208964e9698fac
VimFreeman <github@freiman.cc>
2023-03-14 19:29:28 -0400

    Rehaul bluetooth using library off the internet

---
+++ app/src/main/java/com/jmm/portableairquality/Model/BluetoothHandler.java
@@ -0,0 +1,144 @@
+// code adapted from https://github.com/weliem/blessed-android/blob/master/app/src/main/java/com/welie/blessedexample/BluetoothHandler.java
+package com.jmm.portableairquality.Model;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import org.jetbrains.annotations.NotNull;
+import java.util.UUID;
+
+import com.welie.blessed.BluetoothCentralManager;
+import com.welie.blessed.BluetoothCentralManagerCallback;
+import com.welie.blessed.BluetoothPeripheral;
+import com.welie.blessed.BluetoothPeripheralCallback;
+import com.welie.blessed.BondState;
+import com.welie.blessed.GattStatus;
+import com.welie.blessed.HciStatus;
+
+public class BluetoothHandler {
+    private static final String MAC_ADD = "24:6F:28:1A:72:9E"; //SparkFun Thing MAC Address
+
+    public static final String MEASUREMENT_CCS = "paq.measurement.css";
+    public static final String MEASUREMENT_DHT = "paq.measurement.dht";
+    public static final String MEASUREMENT_PM1 = "paq.measurement.pm1";
+    public static final String MEASUREMENT_PM2 = "paq.measurement.pm2";
+    public static final String MEASUREMENT_PM10 = "paq.measurement.pm10";
+
+    private static final UUID PAQ_SERVICE_UUID = UUID.fromString("87843d21-fd03-4307-8a95-bd3d76b49644");
+
+    private static final UUID CCS_CHAR_UUID = UUID.fromString("9d7af512-e74c-44f8-9974-c9714a8b6847");
+    private static final UUID DHT_CHAR_UUID = UUID.fromString("f78ebbff-c8b7-4107-93de-889a6a06d408");
+    private static final UUID PM1_CHAR_UUID = UUID.fromString("ca73b3ba-39f6-4ab3-91ae-186dc9577d99");
+    private static final UUID PM2_CHAR_UUID = UUID.fromString("168c693b-b9c8-4053-9fde-be87f7d14b72");
+    private static final UUID PM10_CHAR_UUID = UUID.fromString("8e713220-d13a-484d-a251-36d1aa957ecb");
+
+    public BluetoothCentralManager btCentral;
+    private static BluetoothHandler btHandler = null;
+    private final Context context;
+    private final Handler handler = new Handler();
+
+    private final BluetoothPeripheralCallback peripheralCallback = new BluetoothPeripheralCallback() {
+        @Override
+        public void onServicesDiscovered(@NonNull BluetoothPeripheral peripheral) {
+            // turn on notifications for the different characteristics
+            peripheral.setNotify(PAQ_SERVICE_UUID, CCS_CHAR_UUID, true);
+            peripheral.setNotify(PAQ_SERVICE_UUID, DHT_CHAR_UUID, true);
+            peripheral.setNotify(PAQ_SERVICE_UUID, PM1_CHAR_UUID, true);
+            peripheral.setNotify(PAQ_SERVICE_UUID, PM2_CHAR_UUID, true);
+            peripheral.setNotify(PAQ_SERVICE_UUID, PM10_CHAR_UUID, true);
+        }
+
+        @Override
+        public void onCharacteristicUpdate(@NotNull BluetoothPeripheral peripheral, @NotNull byte[] value, @NotNull BluetoothGattCharacteristic characteristic, @NotNull GattStatus status) {
+            if (status != GattStatus.SUCCESS) return;
+
+            UUID characteristicUUID = characteristic.getUuid();
+
+            if (characteristicUUID.equals(CCS_CHAR_UUID)) {
+                //TODO
+            } else if (characteristicUUID.equals(DHT_CHAR_UUID)) {
+                //TODO
+            } else if (characteristicUUID.equals(PM1_CHAR_UUID)) {
+                //TODO
+            } else if (characteristicUUID.equals(PM2_CHAR_UUID)) {
+                //TODO
+            } else if (characteristicUUID.equals(PM10_CHAR_UUID)) {
+                //TODO
+            }
+        }
+    };
+
+    private final BluetoothCentralManagerCallback managerCallback = new BluetoothCentralManagerCallback() {
+        @Override
+        public void onConnectedPeripheral(@NotNull BluetoothPeripheral peripheral) {
+            Log.d("BTModel", "Connected to device");
+        }
+
+        @Override
+        public void onDisconnectedPeripheral(@NotNull final BluetoothPeripheral peripheral, final @NotNull HciStatus status) {
+            Log.d("BTModel", "Disconnected from device");
+
+            // Reconnect to this device when it becomes available again
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    btCentral.autoConnectPeripheral(peripheral, peripheralCallback);
+                }
+            }, 5000);
+        }
+
+        @Override
+        public void onDiscoveredPeripheral(@NotNull BluetoothPeripheral peripheral, @NotNull ScanResult scanResult) {
+            Log.d("BTModel", String.format("Found Peripheral", peripheral.getName()));
+
+            if (peripheral.getAddress() == MAC_ADD && peripheral.getBondState() == BondState.NONE) {
+                btCentral.stopScan();
+                // Create a bond immediately to avoid double pairing popups
+                btCentral.createBond(peripheral, peripheralCallback);
+            } else {
+                btCentral.connectPeripheral(peripheral, peripheralCallback); // only connects to bonded devices?
+            }
+        }
+
+        @Override
+        public void onBluetoothAdapterStateChanged(int state) {
+            if (state == BluetoothAdapter.STATE_ON) {
+                // Bluetooth is on now, start scanning again
+                // Scan for peripherals with a certain service UUIDs
+                btCentral.startPairingPopupHack();
+                startScan();
+            }
+        }
+    };
+
+    public static synchronized BluetoothHandler getInstance(Context context) {
+        if (btHandler == null) {
+            btHandler = new BluetoothHandler(context.getApplicationContext());
+        }
+        return btHandler;
+    }
+
+    private BluetoothHandler(Context context) {
+        this.context = context;
+
+        btCentral = new BluetoothCentralManager(context, managerCallback, new Handler());
+
+        btCentral.startPairingPopupHack();
+        startScan();
+    }
+
+    private void startScan() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                btCentral.scanForPeripheralsWithServices(new UUID[] {PAQ_SERVICE_UUID});
+            }
+        }, 1000);
+    }
+}
+
+
