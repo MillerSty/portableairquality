cd8495b036d7283bfa8e2de11e12da07d0382df8
VimFreeman <github@freiman.cc>
2023-03-19 23:22:03 -0400

    Fix temp/hum graph implementation

--- app/src/main/java/com/jmm/portableairquality/View/HistoryView.java
+++ app/src/main/java/com/jmm/portableairquality/View/HistoryView.java
@@ -75,38 +75,37 @@ public class HistoryView extends AppCompatActivity {
 
     private void updateChart(List<DataEntry> historicalData) { //send this function sorted historical data of arbitrary length
         long present = historicalData.get(historicalData.size()-1).timestamp;
-        List<Entry> co2 = new ArrayList<Entry>();
-        List<Entry> voc = new ArrayList<Entry>();
-        List<Entry> temp = new ArrayList<Entry>();
-        List<Entry> hum = new ArrayList<Entry>();
+        List<Entry> co2 = new ArrayList<>();
+        List<Entry> voc = new ArrayList<>();
+        List<Entry> temp = new ArrayList<>();
+        List<Entry> hum = new ArrayList<>();
         for (int i = 0; i < historicalData.size(); i++) { //represent the data in terms of seconds behind present
             historicalData.get(i).timestamp -= present;
             co2.add(new Entry(historicalData.get(i).timestamp/1000, historicalData.get(i).co2Entry));
             voc.add(new Entry(historicalData.get(i).timestamp/1000, historicalData.get(i).vocEntry));
             temp.add(new Entry(historicalData.get(i).timestamp/1000, historicalData.get(i).tempEntry));
-            voc.add(new Entry(historicalData.get(i).timestamp/1000, historicalData.get(i).humEntry));
+            hum.add(new Entry(historicalData.get(i).timestamp/1000, historicalData.get(i).humEntry));
         }
 
         LineDataSet co2Data = new LineDataSet(co2, "eCO2");
         co2Data.setAxisDependency(YAxis.AxisDependency.LEFT); //set it to the left AXIS
         co2Data.setColor(0xFF4C7C, 200); // set line colour and opacity
-        co2Data.setCircleColor(0xFF4C7C); // this disables the circles for some reason
-        co2Data.setCubicIntensity(0.2f); // i have no idea what this does
+        co2Data.setDrawCircles(false);
 
         LineDataSet vocData = new LineDataSet(voc, "VOCs");
         vocData.setAxisDependency(YAxis.AxisDependency.RIGHT);
         vocData.setColor(0x787EF4, 200);
-        vocData.setCircleColor(0x787EF4);
+        vocData.setDrawCircles(false);
 
         LineDataSet tempData = new LineDataSet(temp, "Temperature");
         tempData.setAxisDependency(YAxis.AxisDependency.RIGHT);
-        tempData.setColor(0x787EF4, 200);
-        tempData.setCircleColor(0x787EF4);
+        tempData.setColor(0x32c3e2, 200);
+        tempData.setDrawCircles(false);
 
         LineDataSet humData = new LineDataSet(hum, "Relative Humidity");
         humData.setAxisDependency(YAxis.AxisDependency.RIGHT);
-        humData.setColor(0x787EF4, 200);
-        humData.setCircleColor(0x787EF4);
+        humData.setColor(0x807fe2, 200);
+        humData.setDrawCircles(false);
 
         List<ILineDataSet> sets_air = new ArrayList<>();
         sets_air.add(co2Data);
